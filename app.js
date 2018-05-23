ca5d68f7d572d490cc8d8c6b9c86339e790f5cbf
Andy0414 <pjh8667@naver.com>
2018-05-24 00:15:21 +0900

    v3

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const session = require('express-session'); // Session
 const MySQLStore = require('express-mysql-session')(session); // MySQL Store
 const passport = require('passport')
 const NaverStrategy = require('passport-naver').Strategy; // Passport Naver
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -39,7 +41,7 @@ passport.serializeUser(function (user, done) {
 }); // 세션 생성
 
 passport.deserializeUser(function (id, done) {
-    done(null,id);
+    done(null, id);
 }); // 세션 확인
 
 passport.use(new NaverStrategy({
@@ -50,7 +52,8 @@ passport.use(new NaverStrategy({
     function (accessToken, refreshToken, profile, done) {
         var user = {
             image: profile._json.profile_image,
-            username: profile._json.nickname
+            username: profile._json.nickname,
+            room: ''
         }
         done(null, user);
     }
@@ -72,11 +75,57 @@ app.get('/naver/callback', passport.authenticate('naver', {
     failureRedirect: '/'
 })); // 네이버 로그인 콜백
 
-app.get('/list',(req,res)=>{
-    // if(!req.user){
-    //     res.redirect('/');
-    // }
-    // else{
+
+var room = [{
+    name: 'OPEN_CHAT',
+    user: []
+}];
+
+app.get('/list', (req, res) => {
+    if (!req.user) {
+        res.redirect('/');
+    }
+    else {
         res.render('list');
-    // }
+
+        io.on('connection', socket => {
+            socket.emit('roomRenewal', { list: room });
+            socket.on('createRoom', data => {
+                if (room.findIndex(x => x.name == data.roomName) == -1 || data.roomName == '') {
+                    console.log(data.roomName)
+                    room.push({
+                        name: data.roomName,
+                        user: []
+                    })
+                    socket.emit('roomRenewal', { list: room });
+                }
+                else {
+                    socket.emit('createRoom_ERROR', {});
+                }
+
+            })
+
+            socket.on('joinRoom', data => {
+                var roomFind = room.findIndex(x => x.name == data.roomName)
+                if (roomFind == -1) {
+                }
+                else {
+                    socket.leaveAll();
+                    for (i in room) {
+                        room[i].user.splice(room[i].user.indexOf(req.user.username), 1);
+                    }
+                    // user에 room 사용해야함
+
+                    room[roomFind].user.push(req.user.username)
+                    socket.join(room[roomFind].name);
+
+                    socket.emit('roomRenewal', { list: room });
+                }
+            })
+
+            socket.on('reqChat',data=>{
+            })
+        });
+    }
 })
+
--- public/list.js
+++ public/list.js
@@ -1,5 +1,8 @@
+var socket = io();
+
 var plus = document.getElementById('roomCreate_v');
 var roomBox = document.getElementById('roomCreate_box');
+var roomList = document.getElementById('roomList');
 
 roomBox.style.visibility = 'hidden';
 roomBox.style.opacity = 0;
@@ -14,5 +17,37 @@ plus.addEventListener('click', () => {
         roomBox.style.visibility = "visible";
         roomBox.style.opacity = 1;
         roomBox.style.bottom = '15vh';
+    }
+})
+
+function createRoom() {
+    var roomName = document.getElementById('createRoom_name').value;
+    socket.emit("createRoom", { roomName: roomName });
+}
+socket.on('createRoom_ERROR', data => {
+    alert('이미 존재하는 방입니다.');
+})
+
+
+socket.on('roomRenewal', data => {
+    roomList.innerHTML = '';
+    for (i in data.list) {
+        var chatRoom = document.createElement('li');
+        chatRoom.innerText = data.list[i].name;
+        chatRoom.className = data.list[i].name;
+        var chatUserCount = document.createElement('p');
+        chatUserCount.innerText = data.list[i].user.length;
+        chatRoom.appendChild(chatUserCount);
+        roomList.appendChild(chatRoom);
+    }
+    function clickRoom(count) {
+        roomList.children[count].addEventListener('click', () => {
+            socket.emit('joinRoom', {
+                roomName: roomList.children[count].className
+            })
+        })
+    }
+    for (var idx = 0; idx < roomList.childElementCount; idx++) {
+        clickRoom(idx);
     }
 })
